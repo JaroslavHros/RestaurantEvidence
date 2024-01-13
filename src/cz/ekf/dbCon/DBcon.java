eaac45436b224c9005493187bc0c7fd5beb46051
JaroslavHros <138136735+JaroslavHros@users.noreply.github.com>
2024-01-13 19:48:20 +0100

    Update DBcon.java

--- src/cz/ekf/dbCon/DBcon.java
+++ src/cz/ekf/dbCon/DBcon.java
@@ -10,11 +10,11 @@ public class DBcon implements AutoCloseable {
 	  AutoCloseable ac ;
 	 // static  String url = "jdbc:mysql://localhost:3306/mydb?useUnicode=true&amp;characterEncoding=utf8";
 	//  static  String username = "root";
-	 // static  String password = "hateTOlose22";
+	 // static  String password = "passwd";
 	 // static String  port = "3306";
 	 // static String hostname = "jdbc:mysql://database-1.c2xqqvzd86ss.us-east-2.rds.amazonaws.com:3306/user","admin", "hateTOlose22";
 	// static String userName = "admin";
-	 // static String password = "hateTOlose22";
+	 // static String password = "passwd";
 	  //static String dbName = "mydb";
 	 // static String url = "jdbc:mysql://" + hostname + ":" + port + "/" + dbName + "?user=" + userName + "&password=" + password;
     public synchronized static Connection createConnection()
@@ -31,7 +31,7 @@ public class DBcon implements AutoCloseable {
         {
             e.printStackTrace();
         }
-        con = DriverManager.getConnection("jdbc:mysql://database-1.c2xqqvzd86ss.us-east-2.rds.amazonaws.com:3306/mydb","admin", "hateTOlose22");
+        con = DriverManager.getConnection("jdbc:mysql://dbURL:3306/mydb","admin", "passwd");
         System.out.println("Post establishing a DB connection - "+con);
     }
     catch (SQLException e)
